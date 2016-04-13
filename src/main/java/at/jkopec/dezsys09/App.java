ffb4e999e60bfef7fa46882dba15d971a82e00f2
jkopec <jkopec@student.tgm.ac.at>
2016-04-13 19:48:28 +0200

    Hauptklasse erstellt

---
+++ src/main/java/at/jkopec/dezsys09/App.java
@@ -0,0 +1,14 @@
+package at.jkopec.dezsys09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by jakubkopec on 06.04.16.
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
